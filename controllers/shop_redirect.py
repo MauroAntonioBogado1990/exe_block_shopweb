6a579c9ef43108edda3e5f605f94821c20b0a8e4
MauroAntonioBogado1990 <mauroantoniobogado@gmail.com>
2025-11-27 13:28:19 -0300

    add update to field

--- controllers/shop_redirect.py
+++ controllers/shop_redirect.py
@@ -4,7 +4,20 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 class WebsiteSaleRedirect(WebsiteSale):
 
-    @http.route(['/shop', '/shop/page/<int:page>'], type='http', auth="public", website=True)
+    # @http.route(['/shop', '/shop/page/<int:page>'], type='http', auth="public", website=True)
+    # def shop(self, page=0, category=None, search='', **kwargs):
+    #     # Si no hay usuario logueado, redirige al login
+    #     if not request.session.uid:
+    #         return request.redirect('/web/login')
+    #     # Si está logueado, llamamos al controlador original
+    #     return super().shop(page=page, category=category, search=search, **kwargs)
+    
+    @http.route([
+        '/shop',
+        '/shop/page/<int:page>',
+        '/shop/category/<model("product.public.category"):category>',
+        '/shop/category/<model("product.public.category"):category>/page/<int:page>',
+    ], type='http', auth="public", website=True)
     def shop(self, page=0, category=None, search='', **kwargs):
         # Si no hay usuario logueado, redirige al login
         if not request.session.uid:
